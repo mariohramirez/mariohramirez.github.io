c899d03adad17bacbc5c34bc610b21f324148b6c
Mario Ramírez <marioh.ramirez@udea.edu.co>
2025-06-27 04:22:52 -0500

    Creacion inicial de la pagina de inicio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
    content: ["./src/**/*.{html,js}"],
    theme: {
-     extend: {},
+     extend: {
+      fontFamily:{
+        bebasneue: ['bebasneue']
+      },
+      colors: {
+        primary: '#ff6c18',
+        secondary: '#f2f1ed',
+        third: '#fbcb0d',
+        fourht: '#763a11',
+        fifth: '#454445'
+      },
+      animation: {
+        scrollLeft: 'scrollLeft 10s linear infinite'
+      },
+      keyframes: {
+        scrollLeft: {
+          '0%' : { transform: 'translateX(0)'},
+          '100%' : { transform: 'translateX(-100%)'}
+        },
+      }
+     },
    },
    plugins: [],
  }
